10950c01f366f20319fd8feb1f7e4809d54f7b6f
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-15 14:27:20 +0000

    grouped routes by controller and pint-ed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/quotes', [\App\Http\Controllers\QuoteController::class, 'getAll']);
-Route::get('/quotes/{id}', [\App\Http\Controllers\QuoteController::class, 'getSingle']);
+Route::controller(QuoteController::class)->group(function () {
+    Route::get('/quotes', 'getAll');
+    Route::get('/quotes/{id}', 'getSingle');
+});
