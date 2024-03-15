d3fe5dd5f93232d47ac577fcbdcf7c293f375471
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-15 13:33:11 +0000

    creating collection app

---
+++ database/migrations/2024_03_15_132546_create_quotes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('character', 50);
+            $table->string('content', 1000);
+            $table->string('episode_name', 50)->nullable();
+            $table->integer('episode_number', 2)->nullable();
+            $table->integer('series_number', 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
