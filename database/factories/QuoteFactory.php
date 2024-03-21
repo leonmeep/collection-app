f0313fae93481503d96cc09a63dca3a7a2aa7bbb
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-21 10:16:47 +0000

    creating test for get all quoutes - not passing - saying episode_name does do exist

--- database/factories/QuoteFactory.php
+++ database/factories/QuoteFactory.php
@@ -17,7 +17,11 @@ class QuoteFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'character' => $this->faker->firstName(50),
+            'words' => $this->faker->paragraph(1000),
+            'episode_name' => $this->faker->text(50),
+            'episode_number' => rand(0, 30),
+            'series_number' => rand(0, 34),
         ];
     }
 }
---
+++ tests/Feature/QuoteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Quote;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class QuoteTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_get_all_quotes(): void
+    {
+        Quote::factory()->count(3)->create();
+
+        $response = $this->getJson('/api/quotes');
+
+        $response->assertStatus(201)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message', 'data'])
+                    ->has('data', 3, function (AssertableJson $json)
+                    {
+                        $json->hasAll(['id', 'character', 'words', 'episode_name',
+                            'episode_number', 'series_number']);
+
+
+                    });
+            });
+    }
+}
